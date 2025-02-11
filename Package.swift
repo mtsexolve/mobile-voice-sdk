7fea40617a2df6f8932573cea7ee6ee74691a90c
Publisher <publisher@mtt.ru>
2025-02-11 08:42:32 +0300

    "Publish version 1.8.0"

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ExolveVoiceSDK",
-            url: "https://github.com/mtsexolve/mobile-voice-sdk/releases/download/1.7.3/ExolveVoiceSDK.xcframework.zip",
-            checksum: "5727aaf3ec8a5adc9e0a0f44891eadf75a0273d346cb9737cf999b710ca47dff"
+            url: "https://github.com/mtsexolve/mobile-voice-sdk/releases/download/1.8.0/ExolveVoiceSDK.xcframework.zip",
+            checksum: "bb19e1224e5ca9e0f1208d26ef86973046ba2665ba85f16692ab0b57a0725dc9"
         )
     ]
 )
